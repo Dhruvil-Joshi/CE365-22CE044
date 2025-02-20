4c37c447070aee70a38e0d9fcac5a7e9b0524f0a
Dhruvil-Joshi <dhruviljoshi1910@gmail.com>
2025-02-20 12:48:20 +0530

    Practical 5 completed

---
+++ Practical_5/demo.c
@@ -0,0 +1,9 @@
+int main() {
+    int a = 5, 7H, @123 , @$12d , 7.a;
+    char b = "x";
+    float c = 7.0;
+    // Single line comment
+    /*This is a multi line comment 
+    This is comment */
+    return a + b;
+}
